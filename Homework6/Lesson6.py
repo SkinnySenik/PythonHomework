5ba5b327706eaf623e4dd9620fb9bee3819af21f
Arseny <senik006kup@gmail.com>
2023-03-06 18:04:18 +0300

    sixth homework of python

---
+++ Homework6/Lesson6.py
@@ -0,0 +1,18 @@
+# 30.
+"""
+a1 = int(input("type the first element:"))
+d = int(input("type the difference: "))
+n = int(input("type the number: "))
+
+for i in range(n):
+    print(a1 + i * d)
+"""
+# 32.
+"""
+list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+min_number = int(input("Type the minimum number: "))
+max_number = int(input("Type the maximum number: "))
+for i in range(len(list_1)):
+    if min_number <= list_1[i] <= max_number:
+        print(i)
+"""
